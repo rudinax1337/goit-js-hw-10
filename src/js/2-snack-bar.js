600d52828c7bf29dcbf2343f06b8f09161ea800c
Illia <illya.bolyukh04@gmail.com>
2026-07-23 21:19:38 +0200

    snackbar

--- src/js/2-snack-bar.js
+++ src/js/2-snack-bar.js
@@ -1,1 +1,42 @@
-console.log('hello');
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const delay = Number(event.currentTarget.elements.delay.value);
+  const state = event.currentTarget.elements.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+
+  event.currentTarget.reset();
+}
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
